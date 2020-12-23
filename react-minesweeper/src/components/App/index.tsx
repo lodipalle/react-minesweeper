1facd0922e534e9d458388f2bac7b2a07d44b2fc
Lodi Palle <lodipalle@gmail.com>
2020-12-24 09:58:35 +1100

    building initial styling components

--- react-minesweeper/src/components/App/index.tsx
+++ react-minesweeper/src/components/App/index.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import "./App.scss";
 
 const App: React.FC = () => {
-  return <div className="App">Hello from App;</div>;
+  return <div className="App">
+    <div className="Header">Header</div>
+    <div className="Body">Body</div>
+  </div>;
 };
 
 export default App;
